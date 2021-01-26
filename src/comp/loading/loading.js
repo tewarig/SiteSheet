37e716b142adc244088766fc7859c5fe99e50102
Gaurav Tewari <gauravtewari111@gmail.com>
2021-01-26 16:05:09 +0530

    Loading comp improved

--- src/comp/loading/loading.js
+++ src/comp/loading/loading.js
@@ -2,15 +2,24 @@ import React from 'react';
 import {CircularProgress} from '@material-ui/core';
 import { makeStyles , Box } from '@material-ui/core/styles'
 import  "./loading.css";
+import Funnies from 'funnies';
+
 
 
 function Loading() {
+  let funnies = new Funnies();
+
   return (
     <>
+   
    <div className="centered ">
-   <CircularProgress  />
-       
-  </div>
+   
+   <h1>
+     {funnies.message()} <br/>
+   <div className="loading">  <CircularProgress color={'#000'}  /> </div>
+   </h1>
+   </div>
+  
   
 
     
