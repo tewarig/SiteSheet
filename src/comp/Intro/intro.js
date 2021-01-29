6a309a010de6401751d244501de3ae4a3368b502
Gaurav Tewari <gauravtewari111@gmail.com>
2021-01-29 21:03:07 +0530

    intro component done

--- src/comp/Intro/intro.js
+++ src/comp/Intro/intro.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import {CircularProgress} from '@material-ui/core';
-import { makeStyles , Box } from '@material-ui/core/styles'
+import { Button} from '@material-ui/core/'
 import Exel from './exel.svg';
 import  "./intro.css";
 import Typewriter from 'typewriter-effect';
 
-
+import {
+   
+    Link,
+    
+  } from "react-router-dom";
 
 function Intro() {
 
@@ -19,15 +22,49 @@ function Intro() {
          </div>
         <img src={Exel} className="intro-exelImg"></img>
         
+
+        <div className="intro-Buttons">
+        
+
+        </div>
+
     <h3 className="intro-typeWrite">
         <Typewriter
         options={{
-        strings: ['API', 'Website','Fun Game'],
-         autoStart: true,
-         loop: true,
-       }}
+            strings: ['API', 'Website','Fun Game'],
+            autoStart: true,
+            loop: true,
+        }}
+        
+        /> 
+        <Link href="/api" >
+        <Button style={{
+            
+            borderRadius: 25,
+            backgroundColor: "#47ff8c",
+            color: "#787c7a",
+            textDecoration: false,
+            marginRight: 5,
 
-             />
+            
+    
+        }} ><h3 className="intro-button-text">Convert into API</h3></Button> 
+        </Link>
+        <Button
+         style={{
+            
+            borderRadius: 25,
+            backgroundColor: "#787c7a",
+            color:"#47ff8c",
+            borderWidth: 5,
+            borderColor: "#787c7a",
+            textDecoration: false,
+            marginRight: 3,
+
+            
+    
+        }}
+        ><h3 className="intro-button-text">Convert into Website</h3></Button> 
         </h3>
         <h3 className="intro-h3">
            Convert Google Sheets into
