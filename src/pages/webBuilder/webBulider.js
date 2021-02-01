26e68d1e2a3af20802eb65a437b3041d16116de1
Gaurav Tewari <gauravtewari111@gmail.com>
2021-02-01 15:32:43 +0530

    basic instrucitons added

--- src/pages/webBuilder/webBulider.js
+++ src/pages/webBuilder/webBulider.js
@@ -2,14 +2,32 @@ import React , {useState}from 'react';
 import './web.css';
 import { Button, TextField } from "@material-ui/core";
 import { makeStyles, fade } from '@material-ui/core/styles';
-
-
 import { ToastContainer, toast } from 'react-toastify';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import { Tooltip } from '@material-ui/core';
+
+
+import {
+   
+  Link,
+  
+} from "react-router-dom";
+
+
+
+
+import './web.css';
 import 'react-toastify/dist/ReactToastify.css';
 
 
-import FileCopyIcon from '@material-ui/icons/FileCopy';
+
+
 import WebImage from './website.svg';
+import YoutubePreview  from '../../assets/web-preview/youtube.webp';
+
+
+
+
 
 const styles =  makeStyles((theme) => ({
   palette: {
@@ -89,15 +107,72 @@ function WebBulider() {
   return (
   <div>
     <div className="web-intro">
-       <h1 className="web-intro-heading"> Convert your google sheet into website  </h1>   
-       <h3 className="web-intro-dis"> Choose your next website from these templates.</h3>
+       <div className="web-intro-heading"> 
+       <h1 className="web-h1-yo">  SiteSheet</h1> <br/> 
+        
+       <span className="web-text-h2">Convert your google sheet into website</span>
+        <br/>
+        <ul  className="web-ul">
+          <li>Make sure your data is according to the format</li>
+          <li> Publish your google sheet to web </li>
+          <li> Choose your favorite template</li>
+          <li> Enter you google sheet url</li>
+          <li> Get Link to your site in a snap.</li>         
+         </ul>
+        </div>   
+       <img src={WebImage} className="web-img" /> 
      </div>
-    {chooseTemplate && <>
-     yo please choose template.
-     <button  onClick={() => (setChooseTemplate(!chooseTemplate))}>
-   yo
-     </button>
-    </>}
+       <h3 className="web-intro-dis"> Choose your next website from these templates.</h3>
+    {chooseTemplate && <div className="web-choose-template">
+  
+      <div className="web-choose-grid"> 
+       <div className="web-preview-card"  >
+       <Tooltip title="Visit">
+         <img src={YoutubePreview} height="300"   className="img-preview-card"/>
+         </Tooltip>
+         <div className="card-text">
+          <h2> Video share </h2>
+          <h3  className="web-card-text-dis">Share your list of youtube videos in a cool format.</h3>
+          <Link to="/api" >
+        <Button style={{
+            
+            borderRadius: 25,
+            backgroundColor: "#47ff8c",
+            color: "#787c7a",
+            textDecoration: false,
+            marginRight: 5,
+
+            
+    
+        }} ><h3 className="intro-button-text">Use this</h3></Button> 
+        </Link>
+        <Link to="/webbuilder/">
+        <Button
+         style={{
+            
+            borderRadius: 25,
+            backgroundColor: "#787c7a",
+            color:"#47ff8c",
+            borderWidth: 5,
+            borderColor: "#787c7a",
+            textDecoration: false,
+            marginRight: 3,
+
+            
+    
+        }}
+        ><h3 className="intro-button-text">See format file</h3></Button> 
+        </Link>
+         </div>
+
+          </div>
+         <div className="web-preview-card">
+         oy </div>
+         <div className="web-preview-card">
+         oy </div>
+      </div>
+
+    </div>}
     {
       !chooseTemplate && <>
       yo this is url window.
