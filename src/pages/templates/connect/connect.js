0bb4c6c2f61fc9f5d33c63d0b613038b922ff5ef
Gaurav Tewari <gauravtewari111@gmail.com>
2021-02-03 19:24:14 +0530

    basic layout added

--- src/pages/templates/connect/connect.js
+++ src/pages/templates/connect/connect.js
@@ -9,6 +9,10 @@ import './connect.css';
 import Coffe from '../../../assets/coffee.svg';
 
 
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
+
 import {
     BrowserRouter as Router,   
     useParams
@@ -57,6 +61,12 @@ export  default function Connect(){
      var userName = data["feed"].entry[0]["content"]['$t'];
      var userImage = data["feed"].entry[1]["content"]['$t'];
      var userEmail = data["feed"].entry[2]["content"]['$t'];
+     var userTwitter = data["feed"].entry[3]["content"]["$t"];
+     var userFacebook = data["feed"].entry[4]["content"]["$t"];
+     var userLinkedIn = data["feed"].entry[5]["content"]["$t"];
+     var userGithub = data["feed"].entry[6]["content"]["$t"];
+
+
 
 
     
@@ -70,6 +80,38 @@ export  default function Connect(){
         <img src={clearText(userImage)} className="connet-image"/>
         <h1>{clearText(userName)} </h1>
         <h2>{clearText(userEmail)}</h2>
+
+
+        <div className="connect-social-icons">
+        {clearText(userTwitter)  && <>  
+            <a href={clearText(userTwitter)}  target="_blank" >
+       <Button>
+           <TwitterIcon/>
+          Twitter 
+         </Button>
+         </a>
+           </>}
+       {clearText(userFacebook) && <> {clearText(userFacebook)} </>  }
+       {clearText(userLinkedIn) && <> 
+        <a href={clearText(userLinkedIn)}  target="_blank" >
+       <Button>
+      <LinkedInIcon/>
+        LinkedIn
+       </Button>
+            </a> 
+        </>}
+       {clearText(userGithub) && <> 
+       <a href={clearText(userGithub)}  target="_blank" >
+       <Button>
+           
+           < GitHubIcon />  <h3>  Github </h3>
+           
+       </Button>
+            </a> 
+            </>}
+
+
+          </div>
          </div>
       
          </>
